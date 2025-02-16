7357b9f44372d7815260cb085f1b958e9ddf1569
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-02-16 15:13:31 +0900

    test: 테스트 실패를 위한 코드 추가

---
+++ src/test/java/org/example/cicdtest/FailTest.java
@@ -0,0 +1,12 @@
+package org.example.cicdtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class FailTest {
+    @Test
+    void alwaysFailTest() {
+        assertThat(true).isFalse();
+    }
+}
