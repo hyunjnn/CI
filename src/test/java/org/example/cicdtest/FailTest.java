6da1e47ea1410cf5c13e3e30f23b4c086c9436f6
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-02-16 15:16:42 +0900

    fix: 실패한 테스트 수정

--- src/test/java/org/example/cicdtest/FailTest.java
+++ src/test/java/org/example/cicdtest/FailTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class FailTest {
     @Test
     void alwaysFailTest() {
-        assertThat(true).isFalse();
+//        assertThat(true).isFalse();
+        assertThat(true).isTrue();
     }
 }
